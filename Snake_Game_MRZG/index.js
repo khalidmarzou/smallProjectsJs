01dba7278e2a68711b4b29698c22a2fc595d48cc
khalid marzoug <KHALIDMARZOUG9@gmail.com>
2024-02-26 08:28:48 +0100

    don't let the snake go out of the container MRZG--0003

--- Snake_Game_MRZG/index.js
+++ Snake_Game_MRZG/index.js
@@ -18,19 +18,33 @@ startBtn.addEventListener("click", () => {
 // control the snake :
 document.addEventListener("keydown", (event) => {
   let snakeRect = snake.getBoundingClientRect();
+  let containerRect = container.getBoundingClientRect();
   let widthSnake = snakeRect.width;
   console.log(snakeRect);
+  console.log(containerRect);
   switch (event.key) {
     case "ArrowUp":
+      if (snakeRect.top - widthSnake < containerRect.top) {
+        return;
+      }
       snake.style.top = snakeRect.top - widthSnake + "px";
       break; // i work with width because in this case i have the same value
     case "ArrowRight":
+      if (snakeRect.left + widthSnake > containerRect.right - widthSnake) {
+        return;
+      }
       snake.style.left = snakeRect.left + widthSnake + "px";
       break;
     case "ArrowDown":
+      if (snakeRect.top + widthSnake > containerRect.bottom - widthSnake) {
+        return;
+      }
       snake.style.top = snakeRect.top + widthSnake + "px";
       break;
     case "ArrowLeft":
+      if (snakeRect.left - widthSnake < containerRect.left) {
+        return;
+      }
       snake.style.left = snakeRect.left - widthSnake + "px";
       break;
     default:
