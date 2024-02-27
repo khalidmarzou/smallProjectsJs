0348a165c2d7e02236e4d98ba6d6c13d2ebd1642
Anasmahfoud <156627676+Anasmahfoud@users.noreply.github.com>
2024-02-27 22:05:18 +0100

    added blur effect

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let aboutUs = document.getElementById("aboutUs");
+let elemstn = document.body.children;
       let linkAboutUs = document.getElementById("linkAboutUs");
       let btnClose = document.getElementById("btnClose");
       linkAboutUs.addEventListener("click", () => {
         aboutUs.classList.remove("d-none");
         aboutUs.style.zIndex = "999";
+        
+          for (let k=1 ; k<= elemstn.length; k++){
+          elemstn[k].classList.add("blur");
+        }
+        
       });
       btnClose.addEventListener("click", () => {
         aboutUs.classList.add("d-none");
+        for (let i=1 ; i<= elemstn.length; i++){
+          elemstn[i].classList.remove("blur");
+        }
       });
+
+console.log(elemstn)
