a03c86651b29db963cdb306079f8b4e2f8a616f6
khalid marzoug <KHALIDMARZOUG9@gmail.com>
2024-02-28 19:29:17 +0100

    just for pull edits from ziko

--- Test.py
+++ Test.py
@@ -1,8 +1,11 @@
 # Open a file in read mode
-file_path = 'K:\JavaScript\smallProjectsJs\KHALID\projects\project2\Test.html'
-openFile = open(file_path, 'r')
-content = openFile.read()
-object = "<object>"
-print(len(content))
-if object in content :
-    print("OK")
+file_path = 'KHALID\projects\project2\Test.html' #make sure that the path is the path of index page
+openHtml = open(file_path, 'r')
+content = openHtml.read()
+object_tag = "</object"
+count_tag_object = content.count(object_tag)
+openHtml.close()
+json_file = "Test.json"
+openJson = open(json_file, 'a')
+openJson.write(f"the objects you have : {count_tag_object}\n")
+openJson.close()
