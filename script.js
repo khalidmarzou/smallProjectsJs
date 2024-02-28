fba634a5b85e850e8299de12dc725efb44926dd9
Zak-65 <sidqui.zakaria1@gmail.com>
2024-02-28 01:18:04 +0100

    page contactUs

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let aboutUs = document.getElementById("aboutUs");
-      let linkAboutUs = document.getElementById("linkAboutUs");
-      let btnClose = document.getElementById("btnClose");
-      linkAboutUs.addEventListener("click", () => {
-        aboutUs.classList.remove("d-none");
-        aboutUs.style.zIndex = "999";
-      });
-      btnClose.addEventListener("click", () => {
-        aboutUs.classList.add("d-none");
-      });
+let linkAboutUs = document.getElementById("linkAboutUs");
+let btnClose = document.getElementById("btnClose");
+linkAboutUs.addEventListener("click", () => {
+  aboutUs.classList.remove("d-none");
+  aboutUs.style.zIndex = "999";
+  aboutUs.style.backdropFilter = "blur(10px)";
+});
+btnClose.addEventListener("click", () => {
+  aboutUs.classList.add("d-none");
+});
